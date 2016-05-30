41bd6974ca224ae26239f2cd5f943dbbc653ab44
Jae S Choi <choijaeseok@gmail.com>
2016-05-30 14:59:12 -0300

    refactor

---
+++ inst/scripts/13.otn.tagging.r
@@ -0,0 +1,17 @@
+redo=F
+if(redo) {
+
+ecomodLibrary('snowcrab')
+
+options(stringsAsFactors=F)
+t13 = read.csv(file.path(project.datadirectory('snowcrab'),'data','tagging','OTN_cabotline_detections_2013.csv'),header=T)
+
+t14 =  read.csv(file.path(project.datadirectory('snowcrab'),'data','tagging','OTN_cabotline_detections_2014.csv'),header=T)
+
+tt = rbind(t13,t14)
+tt1 = tt[which(!tt$sensortype %in% c('release')),]
+
+aggregate(bottom_depth~tagname+receiver,data=tt1,FUN=mean)
+
+
+}
---
+++ inst/scripts/README.md
@@ -0,0 +1,10 @@
+
+NOTE to all:
+
+The "current.assessment.year.r" must be changed every year but must be kept separate from "initialize.local.environment" as running in parallel mode requires occasionally overrding some parameters in "p".
+
+Begin in sequence due to data dependencies starting with:
+```
+ 01.snowcrab.r
+ 02. ...., etc
+```
