9f9a4f147932103f0f6d55a20980d95798fef5fd
Jae S Choi <choijaeseok@gmail.com>
2016-06-06 22:14:57 -0300

    updates

---
+++ inst/papers/paper.alaska.habitat.r
@@ -0,0 +1,56 @@
+
+	p = bio.bio.snowcrab::initialise.local.environment()
+
+  setwd( project.datadirectory("bio.snowcrab") )
+
+  det = bio.snowcrab.db( DS ="det.georeferenced" )
+  set = bio.snowcrab.db( DS ="set.complete" )
+
+  xdet = merge( det, set[,c("trip","set", "t", "z")], by=c("trip", "set"), all.x=T, all.y=F )
+  xdet = xdet[ which( xdet$sex %in% c(female,male) ), ]
+
+  xdet$sex = as.factor( xdet$sex)
+  xdet$size = "small"
+  xdet$size[which( xdet$cw > 95) ] =  "big"
+  xdet$size[which( xdet$cw > 60 & xdet$cw < 95) ] =  "med"
+
+  xdet$size = as.factor(  xdet$size )
+  levels(xdet$mat) = c("Immature", "Mature")
+  levels(xdet$sex) = c("Male", "Female")
+
+
+  setup.lattice.options()
+
+  histogram( ~ t | sex + mat , data = xdet,
+    xlab = "Bottom temperature (C)", type = "density",
+    panel = function(x, ...) {
+      panel.histogram(x, col="white", ...)
+      panel.mathdensity( dmath=dnorm, col = "black", args=list(mean=mean(x),sd=sd(x)))
+   } )
+
+   Pr( "png", dname=file.path(project.datadirectory("bio.snowcrab"), "R"), fname="temp.hist", trim=F , res=144)
+
+
+  histogram( ~ z | sex + mat , data = xdet,
+    xlab = "Bottom temperature (C)", type = "density",
+    panel = function(x, ...) {
+      panel.histogram(x, col="white", ...)
+      panel.mathdensity( dmath=dnorm, col = "black", args=list(mean=mean(x),sd=sd(x)))
+   } )
+
+   Pr( "png", dname=file.path(project.datadirectory("bio.snowcrab"), "R"), fname="temp.hist", trim=F , res=144)
+
+
+
+  histogram( ~ t | sex + size , data = xdet,
+    xlab = "depth", type = "density",
+    panel = function(x, ...) {
+      panel.histogram(x, col="white", ...)
+      panel.mathdensity( dmath=dnorm, col = "black", args=list(mean=mean(x),sd=sd(x)))
+   } )
+`
+   Pr( "png", dname=file.path(project.datadirectory("bio.snowcrab"), "R"), fname="temp.hist", trim=F , res=144)
+
+
+
+
