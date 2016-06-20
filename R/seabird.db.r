bb57f4ac22f68e9e5a932e71f1e1cda7993e0869
Jae S Choi <choijaeseok@gmail.com>
2016-06-19 22:02:46 -0300

    testing seabird

--- R/seabird.db.r
+++ R/seabird.db.r
@@ -171,13 +171,15 @@
 
           Mi = which( sbRAW$seabird_uid == id )
           if (length( Mi) == 0 ) next()
+          if (length( Mi) < 30 ) next()
+
           M = sbRAW[ Mi, ]
 
           settimestamp= rid$timestamp[i]
-          time.gate =  list( t0=settimestamp - dminutes(5), t1=settimestamp + dminutes(10) )
+          time.gate =  list( t0=settimestamp - dminutes(5), t1=settimestamp + dminutes(11) )
 
-          bcp = list(id=id, nr=nrow(M), YR=yr, tdif.min=3, tdif.max=9, time.gate=time.gate, depth.min=20, depth.range=c(-40,20),
-                     smooth.windowsize=5, modal.windowsize=5, noisefilter.trim=0.01, noisefilter.target.r2=0.9, noisefilter.quants=c(0.025, 0.975) )
+          bcp = list(id=id, nr=nrow(M), YR=yr, tdif.min=3, tdif.max=9, time.gate=time.gate, depth.min=20, depth.range=c(-40,20), eps.depth=2,
+                     smooth.windowsize=5, modal.windowsize=5, noisefilter.trim=0.025, noisefilter.target.r2=0.9, noisefilter.quants=c(0.025, 0.975) )
 
           bcp = bottom.contact.parameters( bcp ) # add other default parameters
 
