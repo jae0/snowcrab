dfe9fa7ffc7276f5a337458d11897347590e0167
BradHubley <brad.hubley@dfo-mpo.gc.ca>
2017-02-03 12:54:27 -0400

    fisheries plot

--- R/map.fisheries.data.r
+++ R/map.fisheries.data.r
@@ -1,5 +1,5 @@
 
-map.fisheries.data = function(p, outdir,  FUN, yrs, variable='effort',method='tps',probs=c(0,0.975),log.variable=T,offset,theta=75) {
+map.fisheries.data = function(p, outdir,  FUN, yrs, variable='effort',method='grid',probs=c(0,0.975),log.variable=F,offset,theta=75,pres=10) {
 
   x = logbook.db( DS="logbook" )
   
@@ -16,10 +16,8 @@ map.fisheries.data = function(p, outdir,  FUN, yrs, variable='effort',method='tp
   ler = er
   
   if(missing(offset))offset = er[1]  # offset fot log transformation
-  if(log.variable)ler=log(er+offset)
   if(missing(yrs))yrs=sort(unique(x$year))
         
-  datarange = seq( ler[1], ler[2], length.out=50)
   
   for(i in 1:length(yrs)){
   
@@ -28,14 +26,25 @@ map.fisheries.data = function(p, outdir,  FUN, yrs, variable='effort',method='tp
     xd = subset(x,year==yrs[i],c('plon','plat','z'))
     
     if(method=='tps'){
-      browser()
+      #browser()
+      xd$plon = xd$plon
       xd = aggregate(z~plon+plat,data=xd,FUN)
       if(log.variable)xd$z = log(xd$z+offset)
       u= fastTps(x=xd[,c("plon","plat")] , Y=xd[,'z'], theta=theta )
-      res = cbind( predlocs[,1:2], predict(u, xnew=predlocs[,1:2]))
+      xyz = cbind( predlocs[,1:2], predict(u, xnew=predlocs[,1:2]))
     }
-    xyz = res
+    if(method=='grid'){
+      p$pres=pres
+      g= spatial_grid(p,"planar.coords")
+      xd$plon = grid.internal( xd$plon, g$plon )
+      xd$plat = grid.internal( xd$plat, g$plat )
+      xyz = aggregate(z~plon+plat,data=xd,FUN)
+      ler=quantile( xyz$z, probs=probs)
+    }
+      datarange = seq( ler[1], ler[2], length.out=50)
+
     names( xyz) = c("plon", "plat", "z")
+    cols = colorRampPalette(c( "yellow", "orange","darkred", "black"), space = "Lab")
     cols = colorRampPalette(c("darkblue","cyan","green", "yellow", "orange","darkred", "black"), space = "Lab")
     xyz$z[xyz$z>ler[2]] = ler[2]
     ckey=NULL
@@ -45,8 +54,8 @@ map.fisheries.data = function(p, outdir,  FUN, yrs, variable='effort',method='tp
       labs=labs[which(labs>er[1]&labs<er[2])]
       ckey=list(labels=list(at=log(labs+offset),labels=labs,cex=2))
     }
-    
-    try( map( xyz, xyz.coords="planar", cfa.regions=T, depthcontours=T, pts=set_xyz[,c("plon","plat")], annot=yrs[i], fn=outfn, loc=outloc, at=datarange , col.regions=cols(length(datarange)+1), colpts=F, corners=p$corners, display=F,colorkey=ckey))
+    #browser()
+    try( map( xyz, xyz.coords="planar", cfa.regions=T, depthcontours=T, pts=NULL, annot=yrs[i], fn=outfn, loc=outloc, at=datarange , col.regions=cols(length(datarange)+1), colpts=F, corners=p$corners, display=F,colorkey=ckey, rez=c(pres,pres)))
   }
 }
 
