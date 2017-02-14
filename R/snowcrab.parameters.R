79fa34b56c89f47619a602b67aa8d0a86cb4c5c7
Jae S Choi <choijaeseok@gmail.com>
2017-02-14 15:37:45 -0400

    misc

--- R/snowcrab.parameters.R
+++ R/snowcrab.parameters.R
@@ -100,8 +100,8 @@ snowcrab.parameters = function( p=NULL, DS="default", current.year=NULL, varname
     
     if (!exists("lbm_rsquared_threshold", p)) p$lbm_rsquared_threshold = 0.2 # lower threshold
     if (!exists("lbm_distance_statsgrid", p)) p$lbm_distance_statsgrid = 1 # resolution (km) of data aggregation (i.e. generation of the ** statistics ** )
-    if (!exists("lbm_distance_prediction", p)) p$lbm_distance_prediction = 6 # this is a half window km
-    if (!exists("lbm_distance_scale", p)) p$lbm_distance_scale = 50 # km ... approx guess of 95% AC range 
+    if (!exists("lbm_distance_prediction", p)) p$lbm_distance_prediction = 5 # this is a half window km
+    if (!exists("lbm_distance_scale", p)) p$lbm_distance_scale = 40 # km ... approx guess of 95% AC range 
     if (!exists("lbm_distance_min", p)) p$lbm_distance_min = 3 
     if (!exists("lbm_distance_max", p)) p$lbm_distance_max = 75
   
