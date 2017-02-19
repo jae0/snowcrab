424cca9e1a0841ba31373d08610a45b6dc34be03
Jae S Choi <choijaeseok@gmail.com>
2017-02-19 08:55:49 -0400

    year as factor

--- R/snowcrab.parameters.R
+++ R/snowcrab.parameters.R
@@ -147,8 +147,8 @@ snowcrab.parameters = function( p=NULL, DS="default", current.year=NULL, varname
 
       # this is the time component (mostly) .. space enters as a rough constraint 
       if (!exists("lbm_local_modelformula", p))  p$lbm_local_modelformula = formula( paste(
-        varname, '~ s(yr, bs="ts") + s(cos.w, k=3, bs="ts") + s(sin.w, k=3, bs="ts") ', 
-          ' + s(cos.w, sin.w, yr, bs="ts", k=20) ',
+        varname, '~ as.factor(yr) + s(cos.w, k=3, bs="ts") + s(sin.w, k=3, bs="ts") ', 
+          ' + s(cos.w, sin.w, yr, bs="ts", k=20, by=as.factor(yr)) ',
           ' + s(plon, k=3, bs="ts") + s(plat, k=3, bs="ts") + s(plon, plat, k=20, bs="ts") ' ) )
       if (!exists("lbm_local_model_distanceweighted", p)) p$lbm_local_model_distanceweighted = TRUE
 
