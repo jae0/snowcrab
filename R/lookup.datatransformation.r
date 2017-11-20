9ea814af8be64e4d15e35bc78df4688f7c0098d3
Jae S Choi <choijaeseok@gmail.com>
2017-11-19 20:25:19 -0400

    fix var name lookup

--- R/lookup.datatransformation.r
+++ R/lookup.datatransformation.r
@@ -5,8 +5,8 @@
     log.transform = bio.snowcrab::variable.list.expand("log.transform")
     scaled.centered = bio.snowcrab::variable.list.expand("scaled.centered")
     sn = bio.snowcrab::variable.list.expand("all.data")
-    set = bio.snowcrab::snowcrab.db(DS="set.complete") # base transform characteristics
-    logs = bio.snowcrab::logbook.db(DS='logbook')
+    set = names( bio.snowcrab::snowcrab.db(DS="set.complete") ) # base transform characteristics
+    logs = names( bio.snowcrab::logbook.db(DS='logbook') )
     repository = file.path( project.datadirectory("bio.snowcrab"), "output", "transform.lookup.rdata" )
     return( list( log.transform=log.transform, sn=sn, set=set,logs = logs, repository=repository ) )
 
