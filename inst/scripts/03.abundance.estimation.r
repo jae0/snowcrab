ab5a3a86c069caf721ddf39ac97620eac2dac490
Jae S Choi <choijaeseok@gmail.com>
2017-11-28 14:02:32 -0400

    simplified call to paramaters for stm

--- inst/scripts/03.abundance.estimation.r
+++ inst/scripts/03.abundance.estimation.r
@@ -1,14 +1,18 @@
-  
-  if (!exists("year.assessment")) year.assessment=lubridate::year(Sys.Date()) 
+
+  if (!exists("year.assessment")) {
+    year.assessment=lubridate::year(Sys.Date()) -1
+    year.assessment=lubridate::year(Sys.Date())
+  }
+
   p = bio.snowcrab::load.environment( year.assessment=year.assessment )
 
 
 
 # --------------------------------------------------------------
 #  Ensure the following scripts complete without error:
-#  these external dependencies permit lookup of data, for this script 
+#  these external dependencies permit lookup of data, for this script
 #Require the following:
-#snowcrab.db("complete.redo") 
+#snowcrab.db("complete.redo")
 
 #Run aegis::(inst/scripts/11.surveys.r), need to update year within or run next line
   #system.file(package="aegis", "scripts", "11.surveys.r")
@@ -19,9 +23,9 @@
 #Substrate and bathymetry can be run (as below) if suspect significant changes in one or both of these datasets
 
 #BZ 2017 these lines below can directly run the indicators without goint to run individual scripts
-#  system.file(package="aegis", "scripts", "07.temperature.R")  
-#  system.file(package="aegis", "scripts", "10.lookuptables.r") 
-#  system.file(package="aegis", "scripts", "11.surveys.r") 
+#  system.file(package="aegis", "scripts", "07.temperature.R")
+#  system.file(package="aegis", "scripts", "10.lookuptables.r")
+#  system.file(package="aegis", "scripts", "11.surveys.r")
 #  system.file(package="aegis", "scripts", "16.speciescomposition.R")
 
 
@@ -39,28 +43,27 @@
 # abundance .. positive valued data .. vn = "snowcrab.large.males_abundance"
 
 p = bio.snowcrab::load.environment( year.assessment=year.assessment )
-p$selection=list( 
-  name = "snowcrab.large.males_abundance",
-  type = "abundance",
-  sex=0, # male
-  mat=1, # do not use maturity status in groundfish data as it is suspect .. 
-  spec_bio=bio.taxonomy::taxonomy.recode( from="spec", to="parsimonious", tolookup=2526 ),
-  len= c( 95, 200 )/10, #  mm -> cm ; aegis_db in cm
-  drop.groundfish.data=TRUE # esp from 1970 to 1999 measurement of invertebrates was sporatic .. zero-values are dropped as they are unreliable 
+
+# 11 hrs with these settings,
+p = snowcrab_stm( p=p, DS="parameters",
+  selection=list(
+    name = "snowcrab.large.males_abundance",
+    type = "abundance",
+    sex=0, # male
+    mat=1, # do not use maturity status in groundfish data as it is suspect ..
+    spec_bio=bio.taxonomy::taxonomy.recode( from="spec", to="parsimonious", tolookup=2526 ),
+    len= c( 95, 200 )/10, #  mm -> cm ; aegis_db in cm
+    drop.groundfish.data=TRUE # esp from 1970 to 1999 measurement of invertebrates was sporatic .. zero-values are dropped as they are unreliable
+  ),
+  stm_global_family = gaussian(link=log),
+  stm_local_modelengine = "twostep",
+  stm_twostep_space = "krige",
+  stm_gam_optimizer=c("outer", "bfgs") ,
+  stm_distance_statsgrid = 2, # resolution (km) of data aggregation (i.e. generation of the ** statistics ** )
+  stm_distance_scale = 50
 )
-p$stm_global_family = gaussian(link=log)
-p$stm_local_modelengine = "twostep"
 
-# 11 hrs with these settings
-p$stm_twostep_space = "krige"
-p$stm_gam_optimizer=c("outer", "bfgs") 
-p$stm_distance_statsgrid = 2 # resolution (km) of data aggregation (i.e. generation of the ** statistics ** )
-p$stm_distance_prediction = p$stm_distance_statsgrid * 0.75 # this is a half window km
-p$stm_distance_scale = 50
-
-p = snowcrab_stm( p=p, DS="parameters" )
-
-# o = snowcrab_stm(p=p, DS="stm_inputs" )  # create fields for 
+# o = snowcrab_stm(p=p, DS="stm_inputs" )  # create fields for
 stm( p=p, DATA='snowcrab_stm( p=p, DS="stm_inputs" )', runmode="stage2" ) # 30 min
 
 p = make.list( list( yrs=p$yrs), Y=p )
@@ -70,39 +73,39 @@ snowcrab_stm( p=p, DS="complete.redo" )
 snowcrab_stm( p=p, DS="baseline.redo" )
 snowcrab_stm( p=p, DS="map.all" )
 
-global_model = stm_db( p=p, DS="global_model") 
+global_model = stm_db( p=p, DS="global_model")
 summary( global_model )
 plot(global_model)
 
 #Below lines are just model outputs for comparison sake
-Family: gaussian 
-Link function: identity 
+Family: gaussian
+Link function: identity
 
 Formula:
-snowcrab.large.males_abundance ~ s(t, k = 3, bs = "ts") + s(tmean.climatology, 
-    k = 3, bs = "ts") + s(tsd.climatology, k = 3, bs = "ts") + 
-    s(log(z), k = 3, bs = "ts") + s(log(dZ), k = 3, bs = "ts") + 
-    s(log(ddZ), k = 3, bs = "ts") + s(log(mr), k = 3, bs = "ts") + 
-    s(Npred, k = 3, bs = "ts") + s(smr, k = 3, bs = "ts") + s(log.substrate.grainsize, 
-    k = 3, bs = "ts") + s(ca1, k = 3, bs = "ts") + s(ca2, k = 3, 
+snowcrab.large.males_abundance ~ s(t, k = 3, bs = "ts") + s(tmean.climatology,
+    k = 3, bs = "ts") + s(tsd.climatology, k = 3, bs = "ts") +
+    s(log(z), k = 3, bs = "ts") + s(log(dZ), k = 3, bs = "ts") +
+    s(log(ddZ), k = 3, bs = "ts") + s(log(mr), k = 3, bs = "ts") +
+    s(Npred, k = 3, bs = "ts") + s(smr, k = 3, bs = "ts") + s(log.substrate.grainsize,
+    k = 3, bs = "ts") + s(ca1, k = 3, bs = "ts") + s(ca2, k = 3,
     bs = "ts")
 
 Parametric coefficients:
-            Estimate Std. Error t value Pr(>|t|)    
+            Estimate Std. Error t value Pr(>|t|)
 (Intercept)  5.58131    0.01869   298.6   <2e-16 ***
 ---
 Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
 
 Approximate significance of smooth terms:
-                                 edf Ref.df       F  p-value    
+                                 edf Ref.df       F  p-value
 s(t)                       1.461e+00      2  67.664  < 2e-16 ***
 s(tmean.climatology)       1.441e+00      2  19.462 6.04e-11 ***
 s(tsd.climatology)         1.951e+00      2  15.470 1.17e-07 ***
 s(log(z))                  1.548e+00      2 199.031  < 2e-16 ***
-s(log(dZ))                 8.326e-01      2   3.413  0.00213 ** 
+s(log(dZ))                 8.326e-01      2   3.413  0.00213 **
 s(log(ddZ))                9.484e-01      2   5.210  0.00026 ***
 s(log(mr))                 1.507e+00      2 219.813  < 2e-16 ***
-s(Npred)                   1.360e-08      2   0.000  0.60121    
+s(Npred)                   1.360e-08      2   0.000  0.60121
 s(smr)                     1.734e+00      2  17.919 1.88e-09 ***
 s(log.substrate.grainsize) 1.898e+00      2 189.463  < 2e-16 ***
 s(ca1)                     1.947e+00      2  48.772  < 2e-16 ***
@@ -118,37 +121,28 @@ GCV = 2.4006  Scale est. = 2.3943    n = 6853
 # year.assessment = 2016
 
 p = bio.snowcrab::load.environment( year.assessment=year.assessment )
-p$selection=list( 
-  name = "snowcrab.large.males_presence_absence",
-  type = "presence_absence",
-  sex=0, # male
-  mat=1, # do not use maturity status in groundfish data as it is suspect ..   
-  spec_bio=bio.taxonomy::taxonomy.recode( from="spec", to="parsimonious", tolookup=2526 ),
-  len= c( 95, 200 )/10, #  mm -> cm ; aegis_db in cm
-  drop.groundfish.data=TRUE # esp from 1970 to 1999 measurement of invertebrates was sporatic .. zero-values are dropped as they are unreliable 
+
+
+p = snowcrab_stm( p=p, DS="parameters",
+  selection=list(
+    name = "snowcrab.large.males_presence_absence",
+    type = "presence_absence",
+    sex=0, # male
+    mat=1, # do not use maturity status in groundfish data as it is suspect ..
+    spec_bio=bio.taxonomy::taxonomy.recode( from="spec", to="parsimonious", tolookup=2526 ),
+    len= c( 95, 200 )/10, #  mm -> cm ; aegis_db in cm
+    drop.groundfish.data=TRUE # esp from 1970 to 1999 measurement of invertebrates was sporatic .. zero-values are dropped as they are unreliable
+  ),
+  stm_global_family = binomial(),
+  stm_local_modelengine = "twostep",
+  stm_twostep_space = "krige",
+  stm_gam_optimizer=c("outer", "bfgs"),
+  stm_distance_statsgrid = 2, # resolution (km) of data aggregation (i.e. generation of the ** statistics ** ),
+  stm_distance_scale = 50
 )
 
-p$stm_global_family = binomial()
-
-p$stm_local_modelengine = "twostep"
-p$stm_twostep_space = "krige"
-p$stm_gam_optimizer=c("outer", "bfgs") 
-p$stm_distance_statsgrid = 2 # resolution (km) of data aggregation (i.e. generation of the ** statistics ** )
-p$stm_distance_prediction = p$stm_distance_statsgrid * 0.75 # this is a half window km
-p$stm_distance_scale = 50
-
-
-p = snowcrab_stm( p=p, DS="parameters" )
-
-# o = snowcrab_stm(p=p, DS="stm_inputs" )  # create fields for 
-DATA='snowcrab_stm( p=p, DS="stm_inputs" )'
-
-stm( p=p, DATA=DATA, tasks=c("initiate", "globalmodel") ) # 30 min
-#   stm( p=p, tasks=c( "serial_debug" ) ) # serial mode
-#   stm( p=p, tasks=c( "continue" ) )    
-stm( p=p, tasks=c( "stage1" ) ) #  3 hrs 
-stm( p=p, tasks=c( "stage2" ) ) #   1 hrs
-stm( p=p, tasks=c( "save" ) )
+# o = snowcrab_stm(p=p, DS="stm_inputs" )  # create fields for
+stm( p=p, DATA='snowcrab_stm( p=p, DS="stm_inputs" )', runmode="stage2"  )
 
 p = make.list( list( yrs=p$yrs), Y=p )
 parallel.run( snowcrab_stm, p=p, DS="predictions.redo" ) # warp predictions to other grids
@@ -157,38 +151,38 @@ snowcrab_stm( p=p, DS="complete.redo" )
 snowcrab_stm( p=p, DS="baseline.redo" )
 snowcrab_stm( p=p, DS="map.all" )
 
-global_model = stm_db( p=p, DS="global_model") 
+global_model = stm_db( p=p, DS="global_model")
 summary( global_model )
 plot(global_model)
 
-Family: binomial 
-Link function: logit 
+Family: binomial
+Link function: logit
 
 Formula:
-snowcrab.large.males_presence_absence ~ s(t, k = 3, bs = "ts") + 
-    s(tmean.climatology, k = 3, bs = "ts") + s(tsd.climatology, 
-    k = 3, bs = "ts") + s(log(z), k = 3, bs = "ts") + s(log(dZ), 
-    k = 3, bs = "ts") + s(log(ddZ), k = 3, bs = "ts") + s(log(mr), 
-    k = 3, bs = "ts") + s(Npred, k = 3, bs = "ts") + s(smr, k = 3, 
-    bs = "ts") + s(log.substrate.grainsize, k = 3, bs = "ts") + 
+snowcrab.large.males_presence_absence ~ s(t, k = 3, bs = "ts") +
+    s(tmean.climatology, k = 3, bs = "ts") + s(tsd.climatology,
+    k = 3, bs = "ts") + s(log(z), k = 3, bs = "ts") + s(log(dZ),
+    k = 3, bs = "ts") + s(log(ddZ), k = 3, bs = "ts") + s(log(mr),
+    k = 3, bs = "ts") + s(Npred, k = 3, bs = "ts") + s(smr, k = 3,
+    bs = "ts") + s(log.substrate.grainsize, k = 3, bs = "ts") +
     s(ca1, k = 3, bs = "ts") + s(ca2, k = 3, bs = "ts")
 
 Parametric coefficients:
-            Estimate Std. Error z value Pr(>|z|)    
+            Estimate Std. Error z value Pr(>|z|)
 (Intercept)  0.82108    0.03282   25.02   <2e-16 ***
 ---
 Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
 
 Approximate significance of smooth terms:
-                                 edf Ref.df  Chi.sq  p-value    
+                                 edf Ref.df  Chi.sq  p-value
 s(t)                       1.769e+00      2 105.703  < 2e-16 ***
 s(tmean.climatology)       1.838e+00      2  68.113  < 2e-16 ***
 s(tsd.climatology)         1.910e+00      2  14.130 0.000585 ***
 s(log(z))                  1.569e+00      2 170.503  < 2e-16 ***
-s(log(dZ))                 6.303e-01      2   2.517 0.035988 *  
+s(log(dZ))                 6.303e-01      2   2.517 0.035988 *
 s(log(ddZ))                9.639e-01      2  12.371 6.30e-05 ***
 s(log(mr))                 1.855e+00      2 196.086  < 2e-16 ***
-s(Npred)                   7.202e-05      2   0.000 0.732550    
+s(Npred)                   7.202e-05      2   0.000 0.732550
 s(smr)                     9.897e-01      2  15.089 3.94e-05 ***
 s(log.substrate.grainsize) 1.752e+00      2 213.989  < 2e-16 ***
 s(ca1)                     1.849e+00      2  22.419 4.75e-06 ***
@@ -204,14 +198,14 @@ UBRE = -0.031615  Scale est. = 1         n = 6853
 # collect all predictions into a single file and return:
 p = bio.snowcrab::load.environment( year.assessment=year.assessment )
 
-p$selection=list( 
+p$selection=list(
   name = "snowcrab.large.males_abundance",
   type = "abundance",
   sex=0, # male
-  mat=1, # do not use maturity status in groundfish data as it is suspect .. 
+  mat=1, # do not use maturity status in groundfish data as it is suspect ..
   spec_bio=bio.taxonomy::taxonomy.recode( from="spec", to="parsimonious", tolookup=2526 ),
   len= c( 95, 200 )/10, #  mm -> cm ; aegis_db in cm
-  drop.groundfish.data=TRUE # esp from 1970 to 1999 measurement of invertebrates was sporatic .. zero-values are dropped as they are unreliable 
+  drop.groundfish.data=TRUE # esp from 1970 to 1999 measurement of invertebrates was sporatic .. zero-values are dropped as they are unreliable
 )
 
 p = snowcrab_stm( p=p, DS="parameters" )
@@ -258,8 +252,3 @@ cor(spred,set$snowcrab.large.males_abundance, use="complete.obs")
 # determine presence absence(Y) and weighting(wt)
 #      set$weekno = floor(set$julian / 365 * 52) + 1
 #      set$dyear = floor(set$julian / 365 ) + 1
-
-
-
-
-
