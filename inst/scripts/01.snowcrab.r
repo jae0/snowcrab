3d221fcbba3d5d18692799fecf4348bad6cb9ba0
bzisserson <Ben.Zisserson@dfo-mpo.gc.ca>
2019-12-13 08:50:05 -0400

    update new local lookup table steps in 01.sonowcrab

--- inst/scripts/01.snowcrab.r
+++ inst/scripts/01.snowcrab.r
@@ -108,7 +108,7 @@ if (obtain.database.snapshot) {
 # local lookup trables are required for the following snowcrab.db steps
 
 library(aegis.bathymetry)
-    p = bio.snowcrab::snowcrab_carstm( DS="parameters", assessment.years=1999:year.assessment ) #is this needed??  
+    p = bio.snowcrab::snowcrab_carstm( DS="parameters", assessment.years=1999:year.assessment ) 
     pB = bathymetry_carstm( p=p, DS="parameters_override" )
     M = bathymetry.db( p=pB, DS="aggregated_data", redo=TRUE ) 
     
@@ -119,7 +119,13 @@ library(aegis.substrate)
 library(aegis.temperature)
     pT = temperature_carstm(p=p, DS="parameters_override" )
     M = temperature.db( p=pT, DS="aggregated_data", redo=TRUE )  
+
+#need to create a symlink through windows to the bathymetry file create through aegis.bathymetry
+#run the following line in windows command prompt
     
+#mklink "C:/bio.data/bio.snowcrab/data/bathymetry.canada.east.lonlat.rawdata.rdata" "C:/bio.data/aegis/bathymetry/data/bathymetry.canada.east.lonlat.rawdata.rdata"
+
+#reinitialize parameters specific to the snow crab project    
 p = bio.snowcrab::load.environment( year.assessment=year.assessment ) #reestablish parameters for snowcrab processes
         
 # -------------------------------------------------------------------------------------
