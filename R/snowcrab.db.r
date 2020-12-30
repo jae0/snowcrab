97189fcc497e36c349733947b30437c48ef0d5b0
Jae S Choi <choi.jae.seok@gmail.com>
2020-12-30 17:25:27 -0400

    typo

--- R/snowcrab.db.r
+++ R/snowcrab.db.r
@@ -1010,7 +1010,7 @@ snowcrab.db = function( DS, p=NULL, yrs=NULL, fn.root=NULL) {
         locs = set[ii, c("plon","plat")]
         timestamp = set$timestamp[ii]
         if (! "POSIXct" %in% class(timestamp)  ) timestamp = as.POSIXct( timestamp, tz=tz, origin=lubridate::origin  )
-        BS = temperature_db ( p=p, year.assessment=max(p$yrs) ), DS="aggregated_data" )  # raw data
+        BS = temperature_db ( p=p, year.assessment=max(p$yrs), DS="aggregated_data" )  # raw data
         BT_map = array_map( "ts->1", BS[,c("yr", "dyear")], dims=c(p$ny, p$nw), res=c( 1, 1/p$nw ), origin=c( min(p$yrs), 0) )
         BS_map = array_map( "xy->1", BS[,c("plon","plat")], gridparams=gridparams )
         tstamp = data.frame( yr = lubridate::year(timestamp) )
