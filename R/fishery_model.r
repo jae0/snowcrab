9e5c8209a8ceb8a619fa7f7eab744e753a3862f3
Jae S Choi <choi.jae.seok@gmail.com>
2021-02-22 11:44:53 -0400

    tweaking

--- R/fishery_model.r
+++ R/fishery_model.r
@@ -88,7 +88,7 @@ fishery_model = function(  p,  DS="logistic_model", assessment_years=2000:p$year
         vector <lower=eps> [U] K;
         vector <lower=eps, upper=2> [U] r;
         vector <lower=eps, upper=6> [U] q;
-        vector <lower=eps, upper=3> [U] qs;
+        vector <lower=eps, upper=6> [U] qs;
         vector <lower=eps, upper=(1-eps)> [U] bosd;  // observation error
         vector <lower=eps, upper=(1-eps)> [U] bpsd;  // process error
         vector <lower=eps, upper=(1-eps)> [U] b0;
@@ -152,7 +152,8 @@ fishery_model = function(  p,  DS="logistic_model", assessment_years=2000:p$year
 
         // spring surveys
         for (j in 1:2) {
-          for (i in 1:(ty-1) ){
+          log(Y[1, j]) ~ normal( log( K[j] * fmax( qs[j] * bm[1,j]  - CAT[1,j]/K[j], eps) ), bosd[j] ) ;
+          for (i in 2:(ty-1) ){
             log(Y[i, j]) ~ normal( log( K[j] * fmax( qs[j] * bm[i,j]  - CAT[i-1,j]/K[j], eps) ), bosd[j] ) ;
           }
         }
